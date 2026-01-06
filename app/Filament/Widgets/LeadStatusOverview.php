8ca7d7e6f1ab639c6435f9fee12f0c1ec38a7b39
Rizky Dhani <rizkydhni03@gmail.com>
2026-01-06 16:04:10 +0700

    feat: add Lead Status widget for funnel visualization

---
+++ app/Filament/Widgets/LeadStatusOverview.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Lead;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class LeadStatusOverview extends BaseWidget
+{
+    protected ?string $heading = 'Lead Pipeline Status';
+
+    protected function getStats(): array
+    {
+        $wonCount = Lead::where('status', 'won')->count();
+        $lostCount = Lead::where('status', 'lost')->count();
+        $inProgressCount = Lead::whereNotIn('status', ['new', 'won', 'lost'])->count();
+
+        return [
+            Stat::make('Won Leads', $wonCount)
+                ->description('Converted to customers')
+                ->descriptionIcon('heroicon-m-check-circle')
+                ->color('success')
+                ->url(route('filament.admin.resources.leads.index', ['tableFilters[status][value]' => 'won'])),
+
+            Stat::make('In Progress', $inProgressCount)
+                ->description('Active negotiations/proposals')
+                ->descriptionIcon('heroicon-m-arrow-path')
+                ->color('info')
+                ->url(route('filament.admin.resources.leads.index')),
+
+            Stat::make('Lost Leads', $lostCount)
+                ->description('Dropped opportunities')
+                ->descriptionIcon('heroicon-m-x-circle')
+                ->color('danger')
+                ->url(route('filament.admin.resources.leads.index', ['tableFilters[status][value]' => 'lost'])),
+        ];
+    }
+}
