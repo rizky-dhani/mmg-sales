9599123ff36c3219b5cec8fb3a929241f15022e8
Rizky Dhani <rizkydhni03@gmail.com>
2026-01-15 11:31:51 +0700

    style: Fix formatting in VisitPolicy

--- app/Policies/VisitPolicy.php
+++ app/Policies/VisitPolicy.php
@@ -2,32 +2,9 @@
 
 namespace App\Policies;
 
+use App\Models\User;
+
 class VisitPolicy extends BasePolicy
 {
     protected string $model = 'visit';
-
-    public function viewAny(User $user): bool
-    {
-        return $user->hasRole('Super Admin');
-    }
-
-    public function view(User $user, $model): bool
-    {
-        return $user->hasRole('Super Admin');
-    }
-
-    public function create(User $user): bool
-    {
-        return $user->hasRole('Super Admin');
-    }
-
-    public function update(User $user, $model): bool
-    {
-        return $user->hasRole('Super Admin');
-    }
-
-    public function delete(User $user, $model): bool
-    {
-        return $user->hasRole('Super Admin');
-    }
 }
