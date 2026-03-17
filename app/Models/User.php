203f91db16581f64cb453de72d2823402e3c1fa6
Rizky Dhani <rizkydhni03@gmail.com>
2026-03-17 10:57:48 +0700

    update canAccessPanel for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,7 +76,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->hasAnyRole(['Super Admin', 'Board of Director', 'Head', 'ProductManager', 'JrProductManager', 'ProductExecutive', 'RegionalSalesManager', 'AreaSalesManager', 'Supervisor', 'SalesRepresentative']);
+        return true;
     }
 
     public function department(): BelongsTo
