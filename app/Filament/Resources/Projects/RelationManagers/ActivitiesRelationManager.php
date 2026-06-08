e566c80bc6448789ad1b2855dca6fa4e9b33425f
Rizky Dhani <rizkydhni03@gmail.com>
2026-06-08 15:18:37 +0700

    Add ActivitiesRelationManager for project activities tab

---
+++ app/Filament/Resources/Projects/RelationManagers/ActivitiesRelationManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources\Projects\RelationManagers;
+
+use Carbon\Carbon;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ActivitiesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'activities';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('activity_code')
+            ->columns([
+                TextColumn::make('activity_code')
+                    ->label('Code')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('performed_at')
+                    ->label('Date')
+                    ->formatStateUsing(fn ($state) => $state ? strtoupper(Carbon::parse($state)->translatedFormat('d M Y')) : '-')
+                    ->sortable(),
+
+                TextColumn::make('customer.name')
+                    ->label('Customer')
+                    ->searchable(),
+
+                TextColumn::make('subject')
+                    ->label('Subject')
+                    ->searchable(),
+
+                TextColumn::make('type')
+                    ->badge()
+                    ->formatStateUsing(fn (string $state): string => ucfirst($state))
+                    ->color(fn ($state) => match ($state) {
+                        'Online Meeting', 'In-person Meeting' => 'success',
+                        'Call', 'Messaging' => 'info',
+                        'Demo', 'Presentation' => 'warning',
+                        default => 'gray',
+                    }),
+
+                TextColumn::make('user.name')
+                    ->label('Sales Rep')
+                    ->searchable(),
+
+                TextColumn::make('outcome')
+                    ->label('Status')
+                    ->badge()
+                    ->color(fn ($state): string => match ($state) {
+                        'Interested' => 'success',
+                        'Not Interested' => 'danger',
+                        'No Answer' => 'warning',
+                        'Need more info' => 'info',
+                        default => 'gray',
+                    }),
+            ])
+            ->defaultSort('performed_at', 'desc');
+    }
+}
