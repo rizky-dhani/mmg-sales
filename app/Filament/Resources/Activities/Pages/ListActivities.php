52545a4bab132aa1e68c2a9fe824bab882c4be6a
Rizky Dhani <rizkydhni03@gmail.com>
2026-07-01 09:33:48 +0700

    feat: add outcome filter tabs to ListActivities page

--- app/Filament/Resources/Activities/Pages/ListActivities.php
+++ app/Filament/Resources/Activities/Pages/ListActivities.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Activities\Pages;
 use App\Filament\Resources\Activities\ActivityResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListActivities extends ListRecords
 {
@@ -14,6 +16,21 @@ class ListActivities extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'interested' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('outcome', 'Interested')),
+            'not_interested' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('outcome', 'Not Interested')),
+            'no_answer' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('outcome', 'No Answer')),
+            'need_more_info' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('outcome', 'Need more info')),
         ];
     }
 }
